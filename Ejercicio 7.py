54093a539106e2375c5ff5d9f73b7b25b71474a2
napo <napolimatiase@gmail.com>
2025-05-16 23:28:10 -0300

    crear ejercicio 7

---
+++ Ejercicio 7.py
@@ -0,0 +1,20 @@
+# calcular distancia
+import math
+horas = 0
+velocidad = 0
+distancia = 0
+while True:
+  try:
+    horas = int(input("Ingrese la cantidad de horas:"))
+    break
+  except ValueError:
+    print("Cantidad inválida")
+while True:
+  try:
+    velocidad = int(input("Ingrese la velocidad:"))
+    break
+  except ValueError:
+    print("Cantidad inválida")
+    continue
+distancia = horas * velocidad
+print("Distancia: ", distancia)
