6b3fe97583ec73e050a6a5073a1fa70d74deaec4
napo <napolimatiase@gmail.com>
2025-05-16 23:40:31 -0300

    crear Ejercicio 9

---
+++ Ejercicio 9.py
@@ -0,0 +1,28 @@
+# calcular capital obtenido de la inversión
+inversion = 0
+interes_anual = 0
+años = 0
+capital_obtenido = 0
+while True:
+  try:
+    inversion = float(input("Ingrese cantidad a invertir:"))
+  except ValueError:
+    print("Cantidad inválida")
+    continue
+  break
+while True:
+  try:
+    inversion = float(input("Ingrese interés anual:"))
+  except ValueError:
+    print("Cantidad inválida")
+    continue
+  break
+while True:
+  try:
+    inversion = int(input("Ingrese cantidad de años:"))
+  except ValueError:
+    print("Cantidad inválida")
+    continue
+  break
+capital_obtenido = inversion*interes_anual/100*años
+print("Capital obtenido: ", capital_obtenido)
