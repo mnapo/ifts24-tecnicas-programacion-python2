6ce024b44cf21ca10a63e42c2d558f99fc86ff35
napo <napolimatiase@gmail.com>
2025-05-16 23:24:00 -0300

    crear ejercicio 5

---
+++ Ejercicio 5.py
@@ -0,0 +1,13 @@
+# pedir temperatura en celcius y escribirla en fahrenheit
+import math
+celsius = 0
+fahrenheit = 0
+while True:
+  try:
+    pies = int(input("Ingrese grados celsius:"))
+    break
+  except ValueError:
+    print("Cantidad inválida")
+    continue
+fahrenheit = celsius * 1.8 + 32
+print("Grados Fahrenheit: ", fahrenheit)
