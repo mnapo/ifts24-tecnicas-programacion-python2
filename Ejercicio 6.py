cb9462f479e7a550a9bba8be6810e1e0b5b96095
napo <napolimatiase@gmail.com>
2025-05-16 23:25:49 -0300

    crear ejercicio 6

---
+++ Ejercicio 6.py
@@ -0,0 +1,20 @@
+# pedir horas trabajadas y costo por hora y mostrar pago
+import math
+horas_trabajadas = 0
+costo_hora = 0
+pago = 0
+while True:
+  try:
+    horas_trabajadas = int(input("Ingrese la cantidad de horas trabajadas:"))
+    break
+  except ValueError:
+    print("Cantidad inválida")
+while True:
+  try:
+    costo_hora = int(input("Ingrese el costo por hora:"))
+    break
+  except ValueError:
+    print("Cantidad inválida")
+    continue
+pago = horas_trabajadas * costo_hora
+print("Pago: ", pago)
